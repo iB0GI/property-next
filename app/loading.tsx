befcf6b428b66ddbbd1a29e2ece9710e6073e64a
Marko Bogović <92849901+iB0GI@users.noreply.github.com>
2025-06-11 14:36:29 +0200

    Added Hero search and properties cards

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+'use client';
+import ClipLoader from 'react-spinners/ClipLoader';
+import React from "react";
+
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+
+const loading = ({ loading }: { loading: boolean }) => {
+  return (
+    <ClipLoader
+      color='#3B82F6'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label='Loading Spinner'
+    />
+  );
+};
+export default loading;
